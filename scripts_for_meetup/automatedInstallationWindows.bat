96a52c0351246c56f3e5f49b3758b0ce23862d58
falent <you@example.com>
2018-11-20 15:20:38 +0100

    added new instructions for installation in Windows

--- scripts_for_meetup/automatedInstallationWindows.bat
+++ scripts_for_meetup/automatedInstallationWindows.bat
@@ -1,24 +1,24 @@
 echo on
 echo " Before we start you need to ensure that Git is properly configured to handle line endings."
-call git config --global core.autocrlf true
+git config --global core.autocrlf true &
 
 echo " Deleting local repository if exists"
-call rmdir /Q /S C:\Users\%username%\Documents\googleHomeAssistantExpressNodeJS
+rmdir /Q /S C:\Users\%username%\Documents\googleHomeAssistantExpressNodeJS &
 
 echo "Cloning our repository"
-call git clone https://github.com/falent/googleHomeAssistantExpressNodeJS.git  C:\Users\%username%\Documents\googleHomeAssistantExpressNodeJS
+git clone https://github.com/falent/googleHomeAssistantExpressNodeJS.git  C:\Users\%username%\Documents\googleHomeAssistantExpressNodeJS &
 
 echo "going to your local repository"
-call cd C:\Users\%username%\Documents\googleHomeAssistantExpressNodeJS
+cd C:\Users\%username%\Documents\googleHomeAssistantExpressNodeJS &
 
 echo "Create a new Docker network:"
-call docker network create myNetwork
+docker network create myNetwork &
 
 echo "delete docker image and container"
-docker rm -f myAssistant && docker rmi -f falent/google_home_assistant_express_node_js_server:1
+docker rm -f myAssistant && docker rmi -f falent/google_home_assistant_express_node_js_server:1 &
 
 echo "Runing the ngrok Docker container in your terminal and do not close this tab!"
-call start cmd.exe @cmd /k  "docker run --rm -it --network myNetwork wernight/ngrok ngrok http myAssistant:5000"
+start cmd.exe @cmd /k  "docker run --rm -it --network myNetwork wernight/ngrok ngrok http myAssistant:5000"
 
 echo "Runing a Google Assistant Docker container"
 docker run -v //c/Users/%username%/Documents/googleHomeAssistantExpressNodeJS:/skill -it --network myNetwork --name myAssistant falent/google_home_assistant_express_node_js_server:1
